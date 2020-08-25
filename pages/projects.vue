3d058ae21278b3eaa6d8267c5c1687fb97ebe28e
havvtom <havvtom@gmail.com>
2020-08-25 15:14:22 +0200

    updated icon routes

--- pages/projects.vue
+++ pages/projects.vue
@@ -10,11 +10,11 @@
           <!-- src="~/assets/images/joshhavv.jpg" -->
             <v-row align="center" justify="start">
                 <v-img
-                  :src="require('~/assets/images/mathsnippets.png')"
+                  :src="project.image"
                   lazy-src="https://picsum.photos/id/11/100/60"
                   aspect-ratio="1"
                   class="grey lighten-2 gray"
-                  max-width="546"
+                  max-width="547"
                   max-height="307"
                 >
                   <template v-slot:placeholder>
@@ -49,6 +49,7 @@
     				<v-btn
     					icon
     					class="mr-5"
+    					@click="redirect(project.githubUrl)"
     				>
     					<v-icon>
     						mdi-github
@@ -56,6 +57,7 @@
     				</v-btn>
     				<v-btn
     					icon
+    					@click="redirect(project.websiteUrl)"
     				>
 						<svg style="width:24px;height:24px" viewBox="0 0 24 24">
 						    <path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
@@ -75,9 +77,17 @@
   				{
   					title: 'Mathsnippets', 
   					description: 'Write your mathematics algebra notes in latex on the web.',
-  					languages: ['Laravel', 'Tailwind', 'Nuxt.js']
+  					languages: ['Laravel', 'Tailwind', 'Nuxt.js'],
+  					image: require('~/assets/images/mathsnippets.png'),
+  					websiteUrl: 'http://www.mathsnippets.co.za',
+  					githubUrl: 'https://github.com/havvtom/Snippets-Client'
   				}
   			]
+  		}
+  	},
+  	methods: {
+  		redirect (url) {
+  			window.open( url, '_blank');
   		}
   	}
   }
